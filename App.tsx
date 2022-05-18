29358b82fb70bd43b7f97903b05970837228a94d
sairajkalkundre <sairajkalkundre@gmail.com>
2022-05-18 13:28:24 +0200

    Feat : -Added Reanimated, svg, fastimage,gesture handler

--- App.tsx
+++ App.tsx
@@ -9,107 +9,36 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
+import {SafeAreaView, Text, View} from 'react-native';
+import FastImage from 'react-native-fast-image';
+import {DownArrow} from './src/assets/icons/DownArrow';
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={{flex: 1}}>
+      <View
+        style={{
+          backgroundColor: 'yellow',
+          flexDirection: 'row',
+          height: 50,
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <Text style={{fontSize: 22}}>Chief Minister</Text>
+        <View style={{height: 30, width: 30, marginRight: 10}}>
+          <DownArrow />
         </View>
-      </ScrollView>
+      </View>
+      {/*<FastImage*/}
+      {/*  style={{width: 200, height: 200}}*/}
+      {/*  source={{*/}
+      {/*    uri: 'https://unsplash.it/400/400?image=1',*/}
+      {/*    headers: {Authorization: 'someAuthToken'},*/}
+      {/*    priority: FastImage.priority.normal,*/}
+      {/*  }}*/}
+      {/*  resizeMode={FastImage.resizeMode.contain}*/}
+      {/*/>*/}
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const Home = () => {
+  return <View style={{flex: 1}} />;
+};
+
+export default Home;
